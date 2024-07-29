2e2176bbd12fd64affc864bc4970b98df30c7343
tanveerxz <tanveersingh252008@gmail.com>
2024-07-29 23:30:25 +0100

    v1

---
+++ README.md
@@ -0,0 +1,58 @@
+# 🚀 Tanveer Singh's Portfolio Website
+
+Welcome to my portfolio website! This site is crafted to showcase my skills, projects, and testimonials as a Frontend Developer. Built using modern technologies, it highlights my expertise and provides an easy way for potential clients and employers to connect with me.
+
+## 🛠 Technologies Used
+
+- **Next.js**: A React framework for server-side rendering and static site generation.
+- **TypeScript**: A superset of JavaScript that adds static types for improved development and maintainability.
+- **Tailwind CSS**: A utility-first CSS framework for rapid and custom UI design.
+- **React.js**: A library for building interactive UIs.
+- **Three.js**: A JavaScript library for creating and displaying animated 3D graphics in the browser.
+
+## 🌟 Features
+
+- **Projects**: A showcase of my work, including detailed descriptions and links to live demos or repositories.
+- **Skills**: A comprehensive list of my technical skills and areas of expertise.
+- **Testimonials**: Quotes and feedback from clients and colleagues about my work and collaboration.
+- **Contact Information**: Easy-to-find sections to connect with me via LinkedIn, GitHub, or through contact forms.
+
+## 🚀 Getting Started
+
+To run this project locally, follow these steps:
+
+### 📝 Prerequisites
+
+- [Node.js](https://nodejs.org/) (v14 or higher recommended)
+
+### 📦 Installation
+
+Install the project dependencies using npm:
+
+```bash
+npm install
+```
+
+**Running the Project**
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) in your browser to view the project.
+
+## 📬 Contact
+
+For inquiries or feedback, feel free to reach out via:
+
+- **LinkedIn**: [Tanveer Singh](https://www.linkedin.com/in/tanveerxz/)
+- **GitHub**: [tanveerxz](https://www.github.com/tanveerxz)
+
+## 📄 License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## 🙏 Acknowledgments
+
+- Thanks to the developers and communities behind Next.js, TypeScript, Tailwind CSS, and Three.js for their amazing tools and resources.
+- Special thanks to contributors and reviewers who have helped enhance this website.
---
+++ data/index.ts
@@ -0,0 +1,148 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Banking App",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "TPP - A product Showcase",
+    des: "Elegant ReactJS site highlighting a power bank with a sleek design.",
+    img: "/tpp.png",
+    link: "https://thepoweredproject.co",
+  },
+  {
+    id: 2,
+    title: "Social Media App",
+    des: "Soon👀",
+    img: "/social.jpg",
+    link: "",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "He is exceptional with React and Tailwind. His quick turnaround and ability to craft clean, beautiful interfaces set him apart. I highly recommend Tanveer for his skill and efficiency in frontend development.",
+    name: "Waseef Mohammad Khan",
+    title: "CEO of W-15 Interactive",
+    imgUrl: "/waseef.png",
+  },
+  {
+    quote:
+      "Tanveer created an exceptional product website for my project, showcasing professionalism, creativity, and technical skill. The site is visually stunning, user-friendly, mobile responsive, and includes custom features. Delivered on time, it has significantly increased impressions and mobile conversions. Tanveer's post-launch support is outstanding. Highly recommended!",
+    name: "Shaurya Goel",
+    title: "CEO Of The Powered Project",
+    imgUrl: "/shaurya.png",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Freelance Frontend Developer",
+    desc: "Developed and delivered high-quality, responsive web applications for clients, from initial concept to final deployment.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "UI/UX Design Intern",
+    desc: "Enhanced UI/UX and supported rebranding efforts, including logo design, while managing project deadlines as an intern.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url: "https://github.com/tanveerxz",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    url: "https://x.com/trueblueyt",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    url: "https://linkedin.com/in/tanveerxz",
+  },
+];
